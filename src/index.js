9ae29fab0c2447dcf7d4ee68eb34fe66bd6d1f84
portaTuimil <pabloportatuimil12@gmail.com>
2024-09-29 11:23:48 +0200

    Nav: logic moved to JS and click eventListener implemented.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import './style.css'
 
+const nav = document.querySelector(".navbar");
+const menu = document.querySelector(".menu");
+let menuToggle;
+let menuState = 0;
+let queryState = 1000;
 
+//Mobile Menu:
+function mediaQuery(){
+    let width = window.innerWidth
+
+    if (width <= 580 && queryState != 0){
+        nav.innerHTML = "<h1>Pablo Porta</h1> <div id='menuToggle'> <input class='mediaButton' type='checkbox' /> <span></span> <span></span> <span></span> </div>";
+        menuActivation();
+        queryState = 0;
+
+    } else if (width >= 580 && queryState != 1){
+        nav.innerHTML = "<h1>Pablo Porta</h1> <ul class='nav-links'> <li><a href='#'>Place</a></li> <li><a href='#''>Holder</a></li> <li><a href='#'>Place</a></li> <li><a href='#''>Holder</a></li>";
+        menu.innerHTML = " ";
+        queryState = 1;
+    }
+};
+
+mediaQuery();
+window.addEventListener('resize', function() {mediaQuery()});
+
+function menuActivation(){
+    if (menuState == 0){
+        menuToggle = document.querySelector("#menuToggle"); 
+        menuToggle.addEventListener("click", ()=>{
+            menu.innerHTML = "<ul id='menu'><li><a href='#'>Place</a></li><li><a href='#'>Holder</a></li><li><a href='#'>Place</a></li><li><a href='#'>Holder</a></li></ul>";
+            menuState = 1;
+            menuActivation();
+        });
+        menuState = 1;
+    } else{
+        menuToggle = document.querySelector("#menuToggle"); 
+        menuToggle.addEventListener("click", ()=>{
+            menu.innerHTML = " ";
+            menuState = 0;
+            menuActivation();
+        });
+        menuState = 0;
+    }
+};
