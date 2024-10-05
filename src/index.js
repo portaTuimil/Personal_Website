735cd79f779b94c295fa8ca02b079f6cf77d13d0
portaTuimil <pabloportatuimil12@gmail.com>
2024-10-05 12:34:20 +0200

    Style: colors changed.

--- src/index.js
+++ src/index.js
@@ -43,11 +43,3 @@ function menuActivation(){
     }
 }
 
-//Parallax: https://verpex.com/blog/website-tips/basic-guide-to-parallax-scrolling
-document.addEventListener('scroll', function () {
-    let scrollPosition = window.scrollY;
-
-    document.querySelector('.parallax-layer:nth-child(1)').style.transform = 'translateY(' + (-scrollPosition * 1.4) + 'px)';
-    document.querySelector('.parallax-layer:nth-child(2)').style.transform = 'translateY(' + (-scrollPosition * 0.5) + 'px)';
-    document.querySelector('.parallax-layer:nth-child(3)').style.transform = 'translateY(' + (-scrollPosition * 0.5) + 'px)';
-});
